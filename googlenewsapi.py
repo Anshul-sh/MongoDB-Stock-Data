1f993ce6137a0b89483686646bd6913b2e489a19
sharm192 <sharm192@uwindsor.ca>
2020-07-29 21:14:27 -0400

    Update on 072920 to store data as json

--- googlenewsapi.py
+++ googlenewsapi.py
@@ -2,11 +2,14 @@ from GoogleNews import GoogleNews
 import datetime
 today = datetime.date.today();
 
-# Creates a googlenews object
+
 class GoogleNewsMethods():
+    
+    # Creates a googlenews object
     def __init__(self):    
         self.googlenews = GoogleNews(lang="en")
 
+    # This will return a list of news for perticular stock on a given date 
     def newscollection(self, stock, date):
         self.googlenews.search(stock)
         self.newsList = googlenews.result()
